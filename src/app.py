aea35180ff88b5dcfbe103abb6385f396511880d
Kristina Tabunchik <tabunchik@doanddot.com>
2023-04-02 13:25:17 +0700

    refactor

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 import os.path
+import shutil
 from shutil import make_archive
 from uuid import uuid4
 
+from PIL import Image
 from fastapi import FastAPI, File, UploadFile
 
 from .dto import ResizeParams
@@ -16,19 +18,20 @@ media_path = os.path.join(os.path.dirname(root_dir), 'media')
 
 @app.post("/upload")
 async def root(fileb: UploadFile = File()):
+    id = str(uuid4())
     file_type = fileb.filename.split('.')[-1]
-    out_file_path = os.path.join(media_path, f'{str(uuid4())}.{file_type}')
+    out_file_path = os.path.join(media_path, f'{id}.{file_type}')
     with open(out_file_path, 'wb') as out_file:
         content = await fileb.read()
         out_file.write(content)
-    return {"Result": "OK"}
+    return {"Result": "OK", 'id': id}
 
 
 @app.post('/resize/{filename}')
 def resize(filename: str, resize_params: ResizeParams):
     file_path = os.path.join(media_path, filename)
     filename, filetype = filename.split('.')  # TODO: fix for filename with .
-    dirname = os.path.join(media_path, filename)
+    dirname = os.path.abspath(os.path.join(media_path, filename))
 
     try:
         os.mkdir(dirname)
@@ -45,4 +48,5 @@ def resize(filename: str, resize_params: ResizeParams):
         resize_image = image.resize((size.width, size.height))
         resize_image.save(os.path.join(media_path, filename, resize_image_name))
     make_archive(dirname, 'zip', dirname)
+    shutil.rmtree(dirname)
     return {"Result": "OK"}
